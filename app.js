a8f045a45e71b0ebbde4caea7f595b0e5ca0708c
Felipe <felipe1793@hotmail.com>
2023-09-07 18:02:01 -0300

    criação do add.html, estilização e inserção da view engine

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
 const db = require("./db/connection");
+const path = require("path")
 const bodyParser = require("body-parser");
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log("O express está rodando na porta: " + port)
+    console.log("http://localhost:" + port + "/")
 });
 
 // Body_Parser
 app.use(bodyParser.urlencoded({extended:false}));
+
+// handle bars
+app.set("views", path(__dirname, "views"));
+app.engine("handlebars", exphbs({defaultLayout: 'main'}));
+app.set("view engine", "handlebars")
 
 // DB connection
 db.authenticate().then(() => {
